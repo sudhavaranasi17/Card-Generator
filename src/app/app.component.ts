0062dedf953ed5889d69e6264e18a4d5c4b0100a
Sudha Varanasi <sudha.varanasi@gmail.com>
2021-11-26 01:08:47 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements OnInit {
+  cardObj = {
+    color: '#' + Math.floor(Math.random()*16777215).toString(16)
+  };
+  cards = [];
+
+  ngOnInit() {
+    this.cards.push(this.cardObj);
+  }
+
+  onAdd(e) {
+    const newCard = {
+      color: '#' + Math.random().toString(16).substr(-6)
+    };
+    this.cards.splice(e + 1, 0, newCard);
+  }
+
+  onDelete(e) {
+    this.cards.splice(e, 1);
+  }
 }
